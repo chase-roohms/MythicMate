7b2bd4e04642bdba2a4af4b8b4958de17e61baf3
Chase Roohms <chaseroohms@gmail.com>
2026-01-12 22:16:29 -0600

    Refactor Authenticate class: standardize constant naming conventions for clarity

--- src/main/java/Authenticate/Authenticate.java
+++ src/main/java/Authenticate/Authenticate.java
@@ -7,9 +7,9 @@ public class Authenticate {
     /*==========================================================================================*/
     private static final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
     
-    private final static String password = getEnvVar("MYTHICMATE_PASSWORD", "");
-    private final static String token = getEnvVar("MYTHICMATE_TOKEN", "");
-    private final static String GPTKey = getEnvVar("MYTHICMATE_GPT_KEY", "");
+    private final static String PASSWORD = getEnvVar("MYTHICMATE_PASSWORD", "");
+    private final static String TOKEN = getEnvVar("MYTHICMATE_TOKEN", "");
+    private final static String GPT_KEY = getEnvVar("MYTHICMATE_GPT_KEY", "");
     
     private static String getEnvVar(String key, String defaultValue) {
         String value = dotenv.get(key);
@@ -22,14 +22,14 @@ public class Authenticate {
     /*Login Function                                                                            */
     /*==========================================================================================*/
     public boolean login(String userPassword){
-        return userPassword.equals(password);
+        return userPassword.equals(PASSWORD);
     }
 
     public String getToken(){
-        return token;
+        return TOKEN;
     }
 
     public static String getGPTKey() {
-        return GPTKey;
+        return GPT_KEY;
     }
 }
