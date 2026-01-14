c55cbf2a37c6613a3a3d4488fb1488ce41ec90bc
Chase Roohms <chaseroohms@gmail.com>
2026-01-13 21:43:51 -0600

    fix: Convert scanner and scanner close to try with resources

--- src/main/java/ICommandsHelpers/QueryCommand.java
+++ src/main/java/ICommandsHelpers/QueryCommand.java
@@ -1,14 +1,14 @@
 package ICommandsHelpers;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import Bot.DiscordBot;
 import Functions.MessageSender;
 import Functions.SpellEmbedSender;
 import net.dv8tion.jda.api.entities.channel.unions.MessageChannelUnion;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
 
 public class QueryCommand {
     public static void query(SlashCommandInteractionEvent event, String lookup) {
@@ -24,18 +24,14 @@ public class QueryCommand {
                 .replace("/", "").toLowerCase() + ".txt";
 
         //Try to find and open the file
-        try{
-            File file = new File(DiscordBot.ROOTDIR + "/database/" + commandName + "/" +
-                    filename);
-            Scanner details = new Scanner(file);                    //Scan the file
+        try (Scanner details = new Scanner(new File(DiscordBot.ROOTDIR + "/database/" + commandName + "/" + filename))) {
             if(commandName.equalsIgnoreCase("spell")){
                 event.getHook().sendMessageEmbeds(SpellEmbedSender.getSpellEmbed(details).build()).setEphemeral(true).queue();
             } else {
                 event.getHook().sendMessage("Found it!").queue();
                 MessageSender.sendMessage(chan, toSend, details);       //Send the scanner to message sender
             }
-            details.close();                                        //Close the scanner
-        }catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             event.getHook().sendMessage("Hmm... I'm not seeing any " + commandName +" named: " +
                     lookup + ", check for spelling errors!").setEphemeral(true).queue();
         }
